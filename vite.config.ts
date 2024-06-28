fdb1fd1775c1680d676e39e843e089718f627b7c
opuopu <nurmdopu428@gmail.com>
2024-06-28 19:38:10 +0600

    fix:fixed port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "143.198.197.69",
+    port: 5174,
+  },
+});
