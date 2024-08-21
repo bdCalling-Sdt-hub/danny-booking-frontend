7b2a336b7de5449faa58a54a01b699fb1a4df8c5
opuopu <nurmdopu428@gmail.com>
2024-08-21 10:35:21 +0600

    fix:fixed issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Outlet } from "react-router-dom";
-import Footer from "./component/Footer";
 import Header from "./component/Header";
 function App() {
   return (
     <>
       <Header />
       <Outlet />
-      <section className="bg-[#91DFC5] px-2 lg:px-0">
-        <Footer />
-      </section>
     </>
   );
 }
